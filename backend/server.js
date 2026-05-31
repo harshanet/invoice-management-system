f47cbe9a3f2d5b483dd6bc5d9350ad1b665410f8
Lance Masina <lance@lancemasina.com>
2026-05-31 17:22:38 +0300

    feature branch: added Vehicle model, controller, and routes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/vehicles', require('./routes/vehicleRoutes')); // This line will register the vehicle routes
 //app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
